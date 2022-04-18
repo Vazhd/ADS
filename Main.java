a9982a51c1a98e7a0172697d741990a258694500
Vazhd <89795391+Vazhd@users.noreply.github.com>
2022-04-18 19:22:53 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,21 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
+    //task 2
     public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+        Scanner in = new Scanner(System.in);
 
+        int[] n = new int[4];
+        for (int i = 0; i < n.length; i++) {
+            n[i]=in.nextInt();
+        }
+        list.insertFirst(n[0]);
+        list.insertFirst(n[1]);
+        list.insertFirst(n[2]);
+        list.insertFirst(n[3]);
+
+        list.display();
     }
 
 }
