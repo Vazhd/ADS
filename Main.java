1ff140521160ada9ebcb8f7c02b34fca77f786a5
Vazhd <89795391+Vazhd@users.noreply.github.com>
2022-04-19 21:10:07 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,50 @@ import java.util.Scanner;
 
 public class Main {
     //task 2
-    public static void main(String[] args) {
-        MyLinkedList list = new MyLinkedList();
-        Scanner in = new Scanner(System.in);
-
-        int[] n = new int[4];
-        for (int i = 0; i < n.length; i++) {
-            n[i]=in.nextInt();
-        }
-        list.insertFirst(n[0]);
-        list.insertFirst(n[1]);
-        list.insertFirst(n[2]);
-        list.insertFirst(n[3]);
-
-        list.display();
-    }
-
+   //public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        LinkedList<Integer> node = new LinkedList<>();
+//
+//        for (int i = 0; i < 4; i++) {
+//            node.add(in.nextInt());
+//        }
+//        Answer(node);
+//    }
+//    public static void Answer(LinkedList<Integer> list){
+//        while (list.isEmpty()==false){
+//            System.out.print(list.getLast()+" ");
+//            list.removeLast();
+//        }
+//    }
+    //Task 3
+       //    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        LinkedList<Integer> node = new LinkedList<>();
+//                for (int i = 0; i < 4; i++) {
+//            node.add(in.nextInt());
+//        }
+//        int n = in.nextInt();
+//        System.out.println(node.get(n));
+//
+//    }
+       //Task 4
+       //public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        LinkedList<Integer> node = new LinkedList<>();
+//                for (int i = 0; i < 4; i++) {
+//            node.add(in.nextInt());
+//        }
+//
+//                Answer(node);
+//    }
+//    public static void Answer(LinkedList<Integer> list){
+//        int AAA = 0;
+//        for (int i = 0; i < 4; i++) {
+//            if(AAA!=list.get(i)){
+//                System.out.println(list.get(i) +" ");
+//                AAA=list.get(i);
+//            }else AAA=list.get(i);
+//        }
+//
+//    }
 }
